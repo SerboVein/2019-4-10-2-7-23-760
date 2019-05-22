fc63b1b38354687ade4a7af746d9713ebaf2cbdc
tynnic <tynnic@hotmail.com>
2019-05-22 16:00:34 +0800

    final

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var price = 6;
+
+        if(inputs.distance < 2)
+                price += 6;
+
+        if(inputs.distance > 8)
+                price += 6 * 0.8 + (inputs.distance - 8) * 1.2;
+        else
+                price += (inputs.distance - 2) * 0.8;
+
+        price += inputs.parkTime * 0.25;
+
+        return price.toFixed(0);
 };
