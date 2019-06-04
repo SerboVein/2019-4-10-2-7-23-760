0ebda4647ad331c9be5a473f5b3c50a817a005bd
tynnic <tynnic@hotmail.com>
2019-06-04 17:08:40 +0800

    final passed

--- main/main.js
+++ main/main.js
@@ -2,15 +2,13 @@ module.exports = function main(inputs) {
     // write your code here...
 	var price = 6;
 
-        if(inputs.distance < 2)
-                price += 6;
-
-        if(inputs.distance > 8)
+        if(inputs.distance >= 8)
                 price += 6 * 0.8 + (inputs.distance - 8) * 1.2;
-        else
+        else if(inputs.distance > 2)
                 price += (inputs.distance - 2) * 0.8;
 
         price += inputs.parkTime * 0.25;
 
-        return (parseInt(price.toFixed(0)));
+        return Math.round(price);
+	
 };
